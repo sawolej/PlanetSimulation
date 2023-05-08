4ac128226e4dbac53866b85f320c174861629fc7
sawolej <kuroxian@gmail.com>
2023-05-08 22:58:41 +0200

    initial commit

---
+++ planetsSimulation/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf;
+
+namespace planetsSimulation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private ModelVisual3D CreatePlanetModel(Planet planet)
+        {
+            // Create a sphere geometry for the planet
+            MeshBuilder meshBuilder = new MeshBuilder();
+            meshBuilder.AddSphere(planet.Position, 0.5); // Change 0.5 to the desired planet size
+
+            // Create the planet material (color, texture, etc.)
+            DiffuseMaterial planetMaterial = new DiffuseMaterial(new SolidColorBrush(planet.Color));
+
+            // Create and return the planet 3D model
+            return new ModelVisual3D
+            {
+                Content = new GeometryModel3D(meshBuilder.ToMesh(), planetMaterial)
+            };
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Planet> planets = PlanetSimulator.InitializePlanets();
+            Debug.WriteLine("Planets initialized.");
+
+            foreach (Planet planet in planets)
+            {
+                Debug.WriteLine($"Added {planet.Name} to the viewport.");
+                ModelVisual3D planetModel = CreatePlanetModel(planet);
+                viewport3D.Children.Add(planetModel);
+            }
+        }
+    }
+}
---
+++ planetsSimulation/Planets.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace planetsSimulation
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public double Mass { get; set; }
+        public System.Windows.Media.Color Color { get; set; }
+        public System.Windows.Media.Media3D.Point3D Position { get; set; }
+        public System.Windows.Media.Media3D.Vector3D Velocity { get; set; }
+
+        // Add any other properties and methods as needed
+    }
+
+    public static class PlanetSimulator
+    {
+        public static List<Planet> InitializePlanets()
+        {
+            List<Planet> planets = new List<Planet>();
+
+            // Add planets to the list, for example:
+            planets.Add(new Planet
+            {
+                Name = "Earth",
+                Mass = 5.97E24,
+                Color = System.Windows.Media.Colors.Blue,
+                Position = new System.Windows.Media.Media3D.Point3D(0, 0, 0),
+                Velocity = new System.Windows.Media.Media3D.Vector3D(0, 0, 0)
+            });
+
+            // Add more planets...
+
+            return planets;
+        }
+
+        public static void UpdatePlanets(List<Planet> planets, double timeDelta)
+        {
+            // Update the positions and velocities of the planets based on the simulation logic
+
+            // For example:
+            // 1. Compute gravitational forces between planets
+            // 2. Update velocities based on the forces
+            // 3. Update positions based on the velocities
+        }
+    }
+}
